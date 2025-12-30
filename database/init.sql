b2c8ecd415f6d69ed28302b609b1fe5c89327d8f
Massimo Zironda <massimo@MacBook-Pro-di-Massimo.local>
2025-12-30 21:29:40 +0100

    Fix admin credentials: change username to 'admin' and password to 'admin1234'

--- database/init.sql
+++ database/init.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Default Admin (Password: admin123) - PLEASE CHANGE AFTER IMPORT
-INSERT INTO users (name, email, password, role) VALUES 
-('Admin', 'admin@parsifalyoga.it', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin');
+-- Default Admin (Password: admin1234) - PLEASE CHANGE AFTER IMPORT
+INSERT INTO users (name, email, password, role) VALUES
+('admin', 'admin@parsifalyoga.it', '$2y$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhQe', 'admin');
